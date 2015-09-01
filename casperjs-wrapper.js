80ec3bf7c677075156ddc2d034e14cf685fb280e
Chris Watson <chris.watson@7digital.com>
2015-09-01 16:54:15 +0100

    Use casperjs phantomjs dependency

--- casperjs-wrapper.js
+++ casperjs-wrapper.js
@@ -11,7 +11,7 @@ var args = [
 var opts = {
 	stdio: 'inherit',
 	env: {
-		PHANTOMJS_EXECUTABLE: require('phantomcss/node_modules/phantomjs').path,
+		PHANTOMJS_EXECUTABLE: require('phantomcss/node_modules/casperjs/node_modules/phantomjs').path,
 		HOST: process.env.HOST
 	}
 };
